79cdeb68ac65531505f417822cea608a30d7e05c
PandaComp <pantonio2000@gmail.com>
2022-02-09 22:42:42 -0500

    update success & failure pages

--- app.js
+++ app.js
@@ -3,23 +3,34 @@ const app = express()
 const bodyParser = require('body-parser')
 const request = require('request')
 const https = require('https')
-import api, { aud_id } from './creds'
+// import api, { aud_id } from './creds'
+// const creds = require("./creds")
+// const api = creds.api
+// const aud_id = creds.aud_id
 
 
 app.use(express.static("public")) // inside my html file, I can just refer to the file relative to "public" folder.
 app.use(bodyParser.urlencoded({extended:true})) // now you'll be able to access req.body with app.post
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
+app.get("/", (req, res)=>{ // respond to get request
+    res.sendFile(`${__dirname}/index.html`)
 })
  
-app.post('/', (req, res)=> {
-    const firstName = req.body.fName
+app.post('/failure', (req, res)=>{
+    res.redirect('/') // redirects using app.get to home route
+})
+
+app.post('/success', (req, res)=>{
+    res.redirect('/')
+})
+
+app.post('/', (req, res)=> { // respond to a post request from the form
+    const firstName = req.body.fName // bodyParser allow .body access
     const lastName = req.body.lName
     const email = req.body.email
     console.log(firstName, lastName, email)
     const data = {
-        members : [
+        members : [ // as per API
             {
                 email_address: email, 
                 status: 'subscribed',
@@ -39,8 +50,8 @@ app.post('/', (req, res)=> {
         auth: `paul1:${api}`
     }
 
-    const request = https.request(url, options, (response)=>{
-        if (response.statusCode < 300) {
+    const request = https.request(url, options, (response)=>{ // needs to be assign to a const then call .write(jsonData) & .end()
+        if (response.statusCode === 200) {
             console.log(`STATUS: ${response.statusCode}`);
             console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
             response.setEncoding('utf8');
@@ -51,12 +62,12 @@ app.post('/', (req, res)=> {
                 console.log("Response is done")
             })
             response.on('error', (e)=>{
-                console.log(`something fucked up: ${e}`)
+                console.log(`something seriously messed up: ${e}`)
             })
             res.sendFile(__dirname + "/success.html")
         } else {
             res.sendFile(__dirname + "/failure.html")
-            console.log('bad thing happened')
+            console.log(`a bad thing happened: ${response.statusCode}`)
         }
 
     })
